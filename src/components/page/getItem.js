b375e6fd12c3a3f146d2379ef788b04a3ee535dd
akutanyarobi <akutanyarobi@gmail.com>
2020-05-31 05:58:42 +0700

    add getiem hooks

--- src/components/page/getItem.js
+++ src/components/page/getItem.js
@@ -1,50 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Badge, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 
-class GetItem extends Component {
-  state = {
-    repos: [],
-  };
+const GetItem = () => {
+  const [repos, setRepos] = useState([]);
 
-  componentDidMount() {
-    axios
-      .get(
-        `https://api.github.com/users/iamshaunjp/repos?per_page=6&sort=created:asc`
-      )
-      .then((res) => {
-        const repos = res.data;
-        this.setState({ repos });
-      });
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios(
+        'https://api.github.com/users/iamshaunjp/repos?per_page=6&sort=created:asc'
+      );
 
-  render() {
-    return (
-      <div className="landing-card">
-        <Row>
-          {this.state.repos.map((repo) => (
-            <Col sm={4}>
-              <Card className="mt-3">
-                <Card.Body>
-                  <a id="a-none" href={repo.html_url}>
-                    <Card.Title>{repo.name}</Card.Title>
-                    <Card.Text>{repo.description}</Card.Text>
-                    <Badge variant="light" className="mr-2">
-                      <i className="fas fa-code-branch" />
-                      {repo.forks_count}
-                    </Badge>
-                    <Badge variant="light">
-                      <i className="fas fa-star" /> {repo.stargazers_count}
-                    </Badge>
-                  </a>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
-}
+      setRepos(res.data);
+    };
+
+    fetchData();
+
+    // async function fetchData() {
+    //   const res = await axios(
+    //     'https://api.github.com/users/iamshaunjp/repos?per_page=6&sort=created:asc'
+    //   );
+
+    //   setRepos(res.data);
+    // }
+
+    // fetchData();
+  }, []);
+
+  console.log(repos);
+
+  return (
+    <div>
+      <Row>
+        {/* pertama tambahkan parameter index/item */}
+        {repos.map((repo) => (
+          <Col sm={4} key={repo.id}>
+            <Card className="mt-3">
+              <Card.Body>
+                <a id="a-none" href={repo.html_url}>
+                  <Card.Title>{repo.name}</Card.Title>
+                  <Card.Text>{repo.description}</Card.Text>
+                  <Badge variant="light" className="mr-2">
+                    <i className="fas fa-code-branch" /> {repo.forks_count}
+                  </Badge>
+                  <Badge variant="light">
+                    <i className="fas fa-star" /> {repo.stargazers_count}
+                  </Badge>
+                </a>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
 export default GetItem;
